c01eac60426e6a665648278fb2381335f396b60e
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-22 00:35:45 -0300

    agregamos task de tareas y el interface

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+
+export const TASK = [
+    {
+        id: 1,
+        text: "terminar el primer modulo de angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre bootstrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: "Agosto 5 a las 21:00",
+        reminder: true
+    }
+]
