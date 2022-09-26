7a794834423eae6eb4c026144662c94b751d76cf
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-26 00:46:28 -0300

    Servicio y servidor de tareas

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Observable, of } from 'rxjs'
 import { Task } from '../task';
 import {TASKS} from '../mock-task'
@@ -7,11 +8,16 @@ import {TASKS} from '../mock-task'
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5001/tasks'
+    
+  
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
   getTask(): Observable<Task []> {
-    const tasks = of (TASKS);
-    return tasks
+   
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
