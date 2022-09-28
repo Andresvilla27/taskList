c10c5a3327e4fc986f53d9eeb09bc82fdcec8075
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-28 01:27:41 -0300

    se agrego servicio de actualizacion de base de datos

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs'
 import { Task } from '../task';
 import {TASKS} from '../mock-task'
+
+const httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +26,13 @@ export class TaskService {
    
     return this.http.get<Task[]>(this.apiUrl)
   }
+  deleteTask(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+  updateTaskReminder(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
 }
